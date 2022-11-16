f6664649a6ae33aa2db8cc1f61803d5391da57b6
Sonu1515n <100188440+Sonu1515n@users.noreply.github.com>
2022-11-16 21:10:32 +0530

    Add files via upload

---
+++ pr1_fibbo.py
@@ -0,0 +1,33 @@
+
+############recursive####
+# Python program to display the Fibonacci sequence
+
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 10
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
+
+###without recursive####
+
+first_num = int(input("Enter the first number of the fibonacci series... "))
+second_num = int(input("Enter the second number of the fibonacci series... "))
+num_of_terms = int(input("Enter the number of terms... "))
+print(first_num,second_num)
+print("The numbers in fibonacci series are : ")
+while(num_of_terms-2):
+   third_num = first_num + second_num
+   first_num=second_num
+   second_num=third_num
+   print(third_num)
+   num_of_terms=num_of_terms-1
